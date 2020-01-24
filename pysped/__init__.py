2cd8c149fc2029c198d151dc225f87c742a6934f
Daniel Jorge <daniel.jorge@sticorp.com.br>
2020-01-24 10:46:34 -0300

    Removendo locale

--- pysped/__init__.py
+++ pysped/__init__.py
@@ -40,14 +40,14 @@
 #
 
 import sys
-import locale
+# import locale
 
-if sys.version_info.major == 2:
-    locale.setlocale(locale.LC_ALL, b'pt_BR.UTF-8')
-    locale.setlocale(locale.LC_COLLATE, b'pt_BR.UTF-8')
-else:
-    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
-    locale.setlocale(locale.LC_COLLATE, 'pt_BR.UTF-8')
+# if sys.version_info.major == 2:
+#     locale.setlocale(locale.LC_ALL, b'pt_BR.UTF-8')
+#     locale.setlocale(locale.LC_COLLATE, b'pt_BR.UTF-8')
+# else:
+#     locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+#     locale.setlocale(locale.LC_COLLATE, 'pt_BR.UTF-8')
 
 from pysped.nfe.leiaute import *
 from pysped.cte.leiaute import *
